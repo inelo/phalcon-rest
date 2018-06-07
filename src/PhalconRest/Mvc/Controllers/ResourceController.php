91fb3e3f0c69a33b70e151d275e56c5a445ed11c
mamazur <mamazur@gbox.pl>
2018-06-07 16:11:47 +0200

    setting HTTP Statuses explicitly in ResourceController

--- src/PhalconRest/Mvc/Controllers/ResourceController.php
+++ src/PhalconRest/Mvc/Controllers/ResourceController.php
@@ -3,7 +3,9 @@
 namespace PhalconRest\Mvc\Controllers;
 
 use PhalconRest\Api\ApiResource as ApiResource;
+use PhalconRest\Constants\Services;
 use PhalconRest\Transformers\ModelTransformer;
+use Phalcon\Http\Response\StatusCode;
 
 class ResourceController extends CollectionController
 {
@@ -39,13 +41,68 @@ class ResourceController extends CollectionController
         return null;
     }
 
-    protected function createResourceResponse($item, $meta = null)
+    protected function createResourceResponse($item, $meta = null, int $httpStatus = StatusCode::OK)
     {
-        return $this->createItemResponse($item, $this->getTransformer(), $this->getResource()->getItemKey(), $meta);
+        return $this->createItemResponse(
+            $item,
+            $this->getTransformer(),
+            $this->getResource()->getItemKey(),
+            $meta,
+            $httpStatus
+        );
     }
 
-    protected function createResourceOkResponse($item, $meta = null)
+    protected function createResourceOkResponse($item, $meta = null, int $httpStatus = StatusCode::OK)
     {
+        $this->setResponseHttpStatus($httpStatus);
+
         return $this->createItemOkResponse($item, $this->getTransformer(), $this->getResource()->getItemKey(), $meta);
+    }
+
+    protected function createItemCreatedResponse(string $locationHeader, int $httpStatus = StatusCode::CREATED)
+    {
+        $this->createResponseWithOutContent($locationHeader, $httpStatus);
+    }
+
+    protected function createItemUpdatedResponse(string $locationHeader, int $httpStatus = StatusCode::NO_CONTENT)
+    {
+        $this->createResponseWithOutContent($locationHeader, $httpStatus);
+    }
+
+    protected function createResponseWithOutContent(string $locationHeader, int $httpStatus = StatusCode::NO_CONTENT)
+    {
+        $this->setResponseHttpStatus($httpStatus);
+        $this->setResponseHeader("Location", $locationHeader);
+    }
+
+    protected function createArrayResponse($array, $key, int $httpStatus = StatusCode::OK)
+    {
+        $this->setResponseHttpStatus($httpStatus);
+
+        return parent::createArrayResponse($array, $key);
+    }
+
+    protected function createItemResponse(
+        $item,
+        $transformer,
+        $resourceKey = null,
+        $meta = null,
+        int $httpStatus = StatusCode::OK
+    ) {
+        $this->setResponseHttpStatus($httpStatus);
+
+        return parent::createItemResponse($item, $transformer, $resourceKey, $meta);
+    }
+
+    private function setResponseHttpStatus(int $httpStatus)
+    {
+        $response = $this->di->getShared(Services::RESPONSE);
+        $response->setStatusCode($httpStatus);
+    }
+
+    private function setResponseHeader(string $headerName, string $headerValue)
+    {
+        $response = $this->di->getShared(Services::RESPONSE);
+        $response->setHeader($headerName, $headerValue);
     }
 }
